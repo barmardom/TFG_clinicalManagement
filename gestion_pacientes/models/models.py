55a87e39fa37dfa649d7a124d1c5760c0f155016
Bartolomé Márquez Domínguez <bmardom@gmail.com>
2018-06-17 21:30:52 +0200

    Añadida sistema de visitas
    -Añadida el sistema de visitas relacioanadas a pacientes
    -Solucionado un bug en patologia que hacia que no se mostraran las columnas

--- gestion_pacientes/models/models.py
+++ gestion_pacientes/models/models.py
@@ -69,6 +69,7 @@ class Paciente(models.Model):
     )
     doctor_id = fields.Many2one('res.users', string='Doctor')
     patologia_ids = fields.One2many('gestion_pacientes.patologia', 'paciente_id', string='Patologia')
+    visita_ids = fields.One2many('gestion_pacientes.visita', 'paciente_id', string='Visita')
     
 class Patologia(models.Model):
     _name = 'gestion_pacientes.patologia'
@@ -82,5 +83,35 @@ class Patologia(models.Model):
     descripcion = fields.Text(
         string="Descripción",
         help='Descripción de patologia'
+    )
+    paciente_id = fields.Many2one('gestion_pacientes.paciente', ondelete='cascade', string="Paciente")
+
+class Visita(models.Model):
+    _name = 'gestion_pacientes.visita'
+    _description = 'Visitas'
+    _rec_name = 'asunto'
+
+    fecha = fields.Date(
+        string='Fecha',
+        help='Fecha de la visita',
+        required=True
+    )
+    asunto = fields.Char(
+        string="Asunto",
+        size=20,
+        required=True,
+        help='Tema relacionado con la visita'
+    )
+    descripcion = fields.Text(
+        string="Descripción",
+        required=True,
+        help='Descripción'
+    )
+    tratamiento = fields.Boolean(
+        string='Tratamiento',
+        help='Tratamiento'
+    )
+    pruebas = fields.Char(
+        string='Pruebas'
     )
     paciente_id = fields.Many2one('gestion_pacientes.paciente', ondelete='cascade', string="Paciente")
